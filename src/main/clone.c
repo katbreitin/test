d1b710dd2ff3c2ca92fb283a63819a5a5420e1e4
Coda Phillips <coda.phillips@wisc.edu>
2022-11-17 21:15:11 +0000

    Remove unused code from clone.c, which is only supported on Linux

--- src/main/clone.c
+++ src/main/clone.c
@@ -1,58 +1,14 @@
-#include <linux/sched.h>    /* Definition of struct clone_args */
+#define _GNU_SOURCE
 #include <sched.h>          /* Definition of CLONE_* constants */
 #include <sys/syscall.h>    /* Definition of SYS_* constants */
 #include <unistd.h>
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
-#include <string.h>
-#include <linux/memfd.h>
-#include <sys/mman.h>
 #include <fcntl.h>
 
 
 int getpid_nocache(){
     return syscall(SYS_getpid);
-}
-
-void find_heap_bounds(void ** heap_start, void ** heap_end){
-   char maps_f[255];
-   char * line;
-   size_t linelen = 0;
-   size_t n = 0;
-   FILE * stream;
-
-   sprintf(maps_f, "/proc/%d/maps", getpid_nocache());
-   stream = fopen(maps_f, "r");
-   while(linelen = getline(&line, &n, stream) > 0){
-       if(strstr(line, "[heap]")){
-           printf(line);
-           char * line2;
-           (*heap_start) = (void *) strtol(line, &line2, 16);
-           line2++;
-           (*heap_end) = (void *) strtol(line2, NULL, 16);
-           printf("%x %x\n", (*heap_start), (*heap_end));
-           free(line);
-           n=0;
-           break;
-       }
-       free(line);
-       n=0;
-   }
-}
-
-
-int mmap_heap(){
-    int fd, pid;
-    void * heap_start, *heap_end;
-    // read heap start & end
-    find_heap_bounds(&heap_start, &heap_end);
-    fd = syscall(SYS_memfd_create, "heap", MFD_CLOEXEC);
-    if(fd == -1){
-       return -1;
-    }
-    mmap(heap_start, heap_end-heap_start, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-    return 0;
 }
 
 void reopen_files(int parent_pid){
